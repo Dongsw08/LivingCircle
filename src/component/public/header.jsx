3049cf97b910f262fae2e8fb14d19fc951902a06
Dongsw08 <aedcitt@163.com>
2017-08-11 01:16:01 +0800

    nav complete

--- src/component/public/header.jsx
+++ src/component/public/header.jsx
@@ -1,38 +1,38 @@
 import React , { Component } from 'react';
 import { NavBar , Drawer , List } from 'antd-mobile';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+//import { bindActionCreators } from 'redux';
+//import { connect } from 'react-redux';
+//import PropTypes from 'prop-types';
+import './header.css'
 
 
 export class Header extends Component{
-   constructor(props){
-       super(props);
-       this.state={
-           open:true,
-           
-        };
-    }
+       constructor(props){
+           super(props);
+           this.state={
+           open:false,
+           }
+       }
 
    onMenuClick=()=>{
-        this.setstate({open:!this.state.open});
+        this.setState({open:!this.state.open});
         }
 
    onListClick=()=>{
 
    }     
     
-
+    
     render(){
        const arr=['电影','购物','关于'];
        
         const sidebar = arr.map((i,index)=>{
             return(
-                <List>
+                <List key={index}>
                     <List.Item key={index} onClick={this.onListClick} multipleLine>{i}</List.Item>
                 </List>
             )
-        })
+        });
 
         return(
             <div>  
@@ -40,13 +40,24 @@ export class Header extends Component{
                 leftContent="菜单"
                 rightContent="已购项目"
                 mode="dark"
-                onLeftClick={e => {
-                    e.preventDefault()
+                onLeftClick={
+                    //e.preventDefault()
                     this.onMenuClick
-                    }}
-                >{this.props.title}</NavBar>
+                    }
+                >dd</NavBar>
+                <Drawer
+                  className='my-drawer'
+                  style={{ minHeight: document.documentElement.clientHeight - 200}}
+                  enableDraHandle
+                  contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
+                  sidebar={sidebar}
+                  open={this.state.open}
+                  onOpenChange={this.onMenuClick}
+                >
+                 Click up
+                </Drawer>
             </div>
-        )
+        );
         
         }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore , applyMiddleware} from 'redux';
-import { Provider } from 'react-redux';
+//import { createStore , applyMiddleware} from 'redux';
+//import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 //const store = createStore();
 
 ReactDOM.render(
-    <Provider>
+   // <Provider>
         <App />
-    </Provider>
+   // </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
  
