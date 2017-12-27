c9f0439cb789aaef753a9fc2951af082509237cd
Dongsw <aedcitt@163.com>
2017-12-28 00:24:33 +0800

    purefunction

--- src/component/moviecontent.jsx
+++ src/component/moviecontent.jsx
@@ -3,7 +3,7 @@ import { Card, WingBlank, WhiteSpace,Button} from 'antd-mobile';
 //import {bindActionCreators} from 'redux';
 //import { connect } from 'react-redux';
 
-/*Components*/
+/*Components
 export class MovieContent extends Component{
   constructor(props){
     super(props);
@@ -26,4 +26,19 @@ export class MovieContent extends Component{
   </div>
   )
  } 
+}*/
+
+export const MovieContent = ({props}) => {
+  const {title, src, alt, handleBuyClick} = props;
+  return(
+    <div>
+     <WingBlank size='lg'>
+        <WhiteSpace size='lg' />
+          <Card>
+            <img className="moviePic" src={src} alt={alt} />
+            <Card.Header title={title} extra={<Button  type="primary" inline onClick={handleBuyClick} style={{ marginRight: '0.08rem' }}>购票</Button>} />
+          </Card>
+      </WingBlank>
+  </div>
+  )
 }
